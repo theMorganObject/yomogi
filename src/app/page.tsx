2107458b221c030941e1d8ce81e195f25410c766
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2023-10-12 20:28:10 -1000

    fixes from feature/styling PR#1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,34 +8,34 @@ import Meals from "../components/Meals/Meals";
 import Cart from "../components/Cart/Cart";
 import CartProvider from "../store/CartProvider";
 
-import classes from "./page.module.css";
+import css from "./page.module.css";
 
 export default function Home() {
-  const [cartIsShown, setCartIsShown] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
-  const showCartHandler = () => {
-    setCartIsShown(true);
+  const onOpenCart = () => {
+    setShowCart(true);
   };
 
-  const hideCartHandler = () => {
-    setCartIsShown(false);
+  const onCloseCart = () => {
+    setShowCart(false);
   };
 
   return (
     <CartProvider>
-      <Header onShowCart={showCartHandler} />
-      {cartIsShown && <Cart onClose={hideCartHandler} id="" item="" />}
-      <main className={classes.container}>
+      <Header onShowCart={onOpenCart} />
+      {showCart && <Cart onClose={onCloseCart} />}
+      <main className={css.container}>
         <Image
           src="/img/menu-bg.jpg"
           alt="Wormwood leaves"
-          className={classes.backgroundImage}
+          className={css.backgroundImage}
           layout="fill"
           objectFit="cover"
           objectPosition="center"
         />
-        <h1 className={classes.title}>YoMogi</h1>
-        <p className={classes.subtitle}>Japanese-Fusion Gastropub</p>
+        <h1 className={css.title}>YoMogi</h1>
+        <p className={css.subtitle}>Japanese-Fusion Gastropub</p>
         <Meals />
       </main>
     </CartProvider>
